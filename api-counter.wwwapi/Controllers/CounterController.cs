34ce6f77f3e84df0f4f3e07f0cd8bcf155915309
André Sturesson <andre.m.sturesson@gmail.com>
2024-01-23 12:10:46 +0100

    Core + Extensions

--- api-counter.wwwapi/Controllers/CounterController.cs
+++ api-counter.wwwapi/Controllers/CounterController.cs
@@ -27,20 +27,18 @@ namespace api_counter.wwwapi.Controllers
         //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
         [HttpGet]
         [Route("")]
-        public async Task<IResult> GetAllCounters()
+        public async Task<IResult> GetAllCounters() 
         {
-            //change the number returned in the line below to counter list variable
-            return TypedResults.Ok(1);
+            return TypedResults.Ok(counters);
         }
 
         //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
         [HttpGet]
         [Route("{id}")]
-        public async Task<IResult> GetACounter()
+        public async Task<IResult> GetACounter(int id)
         {
             //write code here replacing the string.Empty
-            var counter = string.Empty;
-           
+            var counter = counters.Find(x => x.Id == id);
             //leave return line the same
             return counter != null ? TypedResults.Ok(counter) : TypedResults.NotFound();
         }
@@ -48,13 +46,20 @@ namespace api_counter.wwwapi.Controllers
         //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.        
         [HttpGet]
         [Route("greaterthan/{number}")]
-        public async Task<IResult> Get()
+        public async Task<IResult> GetGreater(int number)
         {
-            return TypedResults.Ok();
+            List<Counter> greater = counters.Where(x => x.Value > number).ToList();
+            return TypedResults.Ok(greater);
         }
 
         ////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
-      
+        [HttpGet]
+        [Route("lessthan/{number}")]
+        public async Task<IResult> GetLess(int number)
+        {
+            List<Counter> less = counters.Where(x => x.Value < number).ToList();
+            return TypedResults.Ok(less);
+        }
       
 
 
@@ -63,11 +68,35 @@ namespace api_counter.wwwapi.Controllers
         //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
         //return the counter you have increased
-        
+        [HttpGet]
+        [Route("increase/{id}")]
+        public async Task<IResult> GetIncrease(int id)
+        {
+            Counter? counter = counters.Find(x => x.Id == id);
+            if(counter == null) {
+                return Results.NotFound();
+            } else {
+                counter.Value -= 1;
+                return TypedResults.Ok(counter);
+            }
+        }
+      
 
         //Extension #2
         //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
         //return the counter you have decreased
+        [HttpGet]
+        [Route("decrease/{id}")]
+        public async Task<IResult> GetDecrese(int id)
+        {
+            Counter? counter = counters.Find(x => x.Id == id);
+            if(counter == null) {
+                return Results.NotFound();
+            } else {
+                counter.Value -= 1;
+                return TypedResults.Ok(counter);
+            }
+        }
     }
 }
--- api-counter.wwwapi8/Program.cs
+++ api-counter.wwwapi8/Program.cs
@@ -27,36 +27,66 @@ var counters = app.MapGroup("/counters");
 //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
 counters.MapGet("/", () =>
 {
-    return TypedResults.Ok();
+    return TypedResults.Ok(CounterHelper.Counters);
 });
+
 
 
 //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
 counters.MapGet("/{id}", (int id) =>
 {    
-    return TypedResults.Ok(id);
+    Counter? counter = CounterHelper.Counters.Find(x => x.Id == id);
+    if(counter == null) {
+        return Results.NotFound();
+     }
+    return TypedResults.Ok(counter);
 });
 
 //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.        
 counters.MapGet("/greaterthan/{number}", (int number) =>
 {
-    return TypedResults.Ok(number);
+    List<Counter> greater = CounterHelper.Counters.Where(x => x.Value > number).ToList();
+    return TypedResults.Ok(greater);
 });
 
 ////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
-
+counters.MapGet("/lessthan/{number}", (int number) =>
+{
+    List<Counter> less = CounterHelper.Counters.Where(x => x.Value < number).ToList();
+    return TypedResults.Ok(less);
+});
 
 
 //Extension #1
 //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
 //return the counter you have increased
-
+counters.MapGet("/increase/{id}", (int id) =>
+{
+     Counter? counter = CounterHelper.Counters.Find(x => x.Id == id);
+     if(counter == null) {
+        return Results.NotFound();
+     } else {
+        counter.Value += 1;
+        return TypedResults.Ok(counter);
+     }
+});
 
 //Extension #2
 //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
 //return the counter you have decreased
+counters.MapGet("/decrease/{id}", (int id) =>
+{
+     Counter? counter = CounterHelper.Counters.Find(x => x.Id == id);
+     if(counter == null) {
+        return Results.NotFound();
+     } else {
+        counter.Value -= 1;
+        return TypedResults.Ok(counter);
+     }
+});
 
 app.Run();
+
 
