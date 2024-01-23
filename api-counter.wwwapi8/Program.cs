2b87c219ee0b7b1ff3436b4aed4b7bd6124bbddf
Najem Hamo <najem.hamo@gmail.com>
2024-01-23 14:03:06 +0100

    Done

--- api-counter.wwwapi8/Program.cs
+++ api-counter.wwwapi8/Program.cs
@@ -27,23 +27,26 @@ var counters = app.MapGroup("/counters");
 //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
 counters.MapGet("/", () =>
 {
-    return TypedResults.Ok();
+    return TypedResults.Ok(CounterHelper.Counters);
 });
 
 
 //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
 counters.MapGet("/{id}", (int id) =>
-{    
-    return TypedResults.Ok(id);
+{
+    return TypedResults.Ok(CounterHelper.Counters.FirstOrDefault(c => c.Id == id));
 });
 
 //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.        
 counters.MapGet("/greaterthan/{number}", (int number) =>
 {
-    return TypedResults.Ok(number);
+    return TypedResults.Ok(CounterHelper.Counters.Where(c => c.Value > number).ToList());
 });
 
 ////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
+counters.MapGet("/LessThan/{number}", (int number) =>{
+    return TypedResults.Ok(CounterHelper.Counters.Where(c => c.Value < number).ToList());
+});
 
 
 
@@ -51,12 +54,24 @@ counters.MapGet("/greaterthan/{number}", (int number) =>
 //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
 //return the counter you have increased
+counters.MapPut("/Increment/{id}", (int id) =>
+{
+    var counter = CounterHelper.Counters.FirstOrDefault(c => c.Id == id);
+    counter.Value++;
+    return TypedResults.Ok(counter);
+});
 
 
 //Extension #2
 //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
 //return the counter you have decreased
+counters.MapPut("/Decrement/{id}", (int id) =>
+{
+    var counter = CounterHelper.Counters.FirstOrDefault(c => c.Id == id);
+    counter.Value--;
+    return TypedResults.Ok(counter);
+});
 
 app.Run();
 
