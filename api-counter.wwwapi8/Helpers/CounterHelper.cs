39244487eadbe6d7169a5be7252785437ff6d620
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-01-23 13:05:13 +0100

    Core + Extension

--- api-counter.wwwapi8/Helpers/CounterHelper.cs
+++ api-counter.wwwapi8/Helpers/CounterHelper.cs
@@ -9,13 +9,13 @@ namespace api_counter.wwwapi8.Helpers
 
         public static void Initialize()
         {
-            if (Counters.Count == 0)
+            if(Counters.Count == 0)
             {
-                Counters.Add(new Counter() { Id = 1, Name = "Books", Value = 5 });
-                Counters.Add(new Counter() { Id = 2, Name = "Toys", Value = 2 });
-                Counters.Add(new Counter() { Id = 3, Name = "Videogames", Value = 8 });
-                Counters.Add(new Counter() { Id = 4, Name = "Pencils", Value = 3 });
-                Counters.Add(new Counter() { Id = 5, Name = "Notepads", Value = 7 });
+                Counters.Add(new Counter() { Id = 1 , Name = "Books" , Value = 5 });
+                Counters.Add(new Counter() { Id = 2 , Name = "Toys" , Value = 2 });
+                Counters.Add(new Counter() { Id = 3 , Name = "Videogames" , Value = 8 });
+                Counters.Add(new Counter() { Id = 4 , Name = "Pencils" , Value = 3 });
+                Counters.Add(new Counter() { Id = 5 , Name = "Notepads" , Value = 7 });
             }
         }
         public static List<Counter> Counters { get; set; } = new List<Counter>();
