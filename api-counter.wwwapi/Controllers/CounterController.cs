b2ba4240eaaac6647ff8980a1f7db650409fc55d
SBjer1 <sebastian.engan@no.experis.com>
2024-01-23 15:04:29 +0100

    Done both exercises

--- api-counter.wwwapi/Controllers/CounterController.cs
+++ api-counter.wwwapi/Controllers/CounterController.cs
@@ -1,6 +1,7 @@
 ﻿using api_counter.wwwapi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace api_counter.wwwapi.Controllers
 {
@@ -30,17 +31,22 @@ namespace api_counter.wwwapi.Controllers
         public async Task<IResult> GetAllCounters()
         {
             //change the number returned in the line below to counter list variable
-            return TypedResults.Ok(1);
+
+            return TypedResults.Ok(CounterController.counters);
         }
 
         //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
         [HttpGet]
         [Route("{id}")]
-        public async Task<IResult> GetACounter()
+        public async Task<IResult> GetACounter(int id)
         {
             //write code here replacing the string.Empty
-            var counter = string.Empty;
-           
+            var counter = counters.FirstOrDefault(counters => counters.Id == id);
+            if (counter == null)
+            {
+                return Results.NotFound($"Id: {id} not found!");
+            }
+
             //leave return line the same
             return counter != null ? TypedResults.Ok(counter) : TypedResults.NotFound();
         }
@@ -48,26 +54,77 @@ namespace api_counter.wwwapi.Controllers
         //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.        
         [HttpGet]
         [Route("greaterthan/{number}")]
-        public async Task<IResult> Get()
+        public async Task<IResult> Get(int number)
         {
-            return TypedResults.Ok();
+            List<Counter> valueList = new List<Counter>();
+            foreach (var counter in counters)
+            {
+                if(counter.Value > number)
+                {
+                    valueList.Add(counter);
+                }
+            }
+            
+            return TypedResults.Ok(valueList);
         }
 
         ////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
-      
-      
+        [HttpGet]
+        [Route("lessthan/{number}")]
+        public async Task<IResult> GetValueLessThan(int number)
+        {
+            List<Counter> valueList = new List<Counter>();
+            foreach (var counter in counters)
+            {
+                if (counter.Value < number)
+                {
+                    valueList.Add(counter);
+                }
+            }
 
+            return TypedResults.Ok(valueList);
+        }
 
 
         //Extension #1
         //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
         //return the counter you have increased
-        
+        [HttpGet]
+        [Route("incrementById/{id}")]
+        public async Task<IResult> IncrementValueById(int id)
+        {
+            foreach (var counter in counters)
+            {
+                if (counter.Id == id)
+                {
+                   counter.Value++;
+                }
+            }
+
+            var aCounter = counters.FirstOrDefault(counters => counters.Id == id);
+            return TypedResults.Ok(aCounter);
+        }
+
 
         //Extension #2
         //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
         //return the counter you have decreased
+        [HttpGet]
+        [Route("decrementById/{id}")]
+        public async Task<IResult> DecrementValueById(int id)
+        {
+            foreach (var counter in counters)
+            {
+                if (counter.Id == id)
+                {
+                    counter.Value--;
+                }
+            }
+
+            var aCounter = counters.FirstOrDefault(counters => counters.Id == id);
+            return TypedResults.Ok(aCounter);
+        }
     }
 }
