cfe703f4ff59190e41186099fe9b508f4446c096
panfi98 <danielpanf@gmail.com>
2024-08-28 16:32:20 +0200

    finished with api8 need to adjust previous version api

--- api-counter.wwwapi8/Program.cs
+++ api-counter.wwwapi8/Program.cs
@@ -1,4 +1,5 @@
 using api_counter.wwwapi8;
+using api_counter.wwwapi8.Endpoints;
 using api_counter.wwwapi8.Helpers;
 using api_counter.wwwapi8.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -21,42 +22,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-CounterHelper.Initialize();
-
-var counters = app.MapGroup("/counters");
-//TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
-counters.MapGet("/", () =>
-{
-    return TypedResults.Ok();
-});
-
-
-//TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
-counters.MapGet("/{id}", (int id) =>
-{    
-    return TypedResults.Ok(id);
-});
-
-//TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.        
-counters.MapGet("/greaterthan/{number}", (int number) =>
-{
-    return TypedResults.Ok(number);
-});
-
-////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
-
-
-
-//Extension #1
-//TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
-//e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
-//return the counter you have increased
-
-
-//Extension #2
-//TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
-//e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
-//return the counter you have decreased
+app.CofigureCounterEndpoint();
 
 app.Run();
 
