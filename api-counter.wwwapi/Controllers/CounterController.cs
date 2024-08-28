6b270917fe9ff4ca3f4b1abe5511aee9eff83f0d
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-08-28 09:36:45 +0200

    CounterController Core and Extensions done!

--- api-counter.wwwapi/Controllers/CounterController.cs
+++ api-counter.wwwapi/Controllers/CounterController.cs
@@ -27,19 +27,22 @@ namespace api_counter.wwwapi.Controllers
         //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
         [HttpGet]
         [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IResult> GetAllCounters()
         {
             //change the number returned in the line below to counter list variable
-            return TypedResults.Ok(1);
+            return TypedResults.Ok(counters);
         }
 
         //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
         [HttpGet]
         [Route("{id}")]
-        public async Task<IResult> GetACounter()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IResult> GetACounter(int id)
         {
             //write code here replacing the string.Empty
-            var counter = string.Empty;
+            var counter = counters.FirstOrDefault(x => x.Id == id);
            
             //leave return line the same
             return counter != null ? TypedResults.Ok(counter) : TypedResults.NotFound();
@@ -48,14 +51,24 @@ namespace api_counter.wwwapi.Controllers
         //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.        
         [HttpGet]
         [Route("greaterthan/{number}")]
-        public async Task<IResult> Get()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IResult> Get(int number)
         {
-            return TypedResults.Ok();
+            var counterList = counters.Where(x => x.Value > number);
+            return TypedResults.Ok(counterList);
         }
 
         ////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
-      
-      
+        [HttpGet]
+        [Route("lessthan/{number}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IResult> GetLessThan(int number)
+        {
+            var counterList = counters.Where(x => x.Value < number);
+            return TypedResults.Ok(counterList);
+        }
+
 
 
 
@@ -63,11 +76,36 @@ namespace api_counter.wwwapi.Controllers
         //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
         //return the counter you have increased
-        
+        [HttpGet]
+        [Route("incrementvalue/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IResult> Increment(int id)
+        {
+            var counter = counters.FirstOrDefault(x => x.Id == id);
+            if (counter != null)
+            {
+                counter.Value += 1;
+            }
+            return counter != null ? TypedResults.Ok(counter) : TypedResults.NotFound();
+        }
 
         //Extension #2
         //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
         //return the counter you have decreased
+        [HttpGet]
+        [Route("decrementvalue/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IResult> Decrement(int id)
+        {
+            var counter = counters.FirstOrDefault(x => x.Id == id);
+            if (counter != null)
+            {
+                counter.Value -= 1;
+            }
+            return counter != null ? TypedResults.Ok(counter) : TypedResults.NotFound();
+        }
     }
 }
