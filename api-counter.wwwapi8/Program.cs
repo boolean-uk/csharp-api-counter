07bbf9ba99fd6eefba2fcec28a10e5316f155a08
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-08-28 09:49:33 +0200

    Completed Core and Extension for wwwapi8 as well!

--- api-counter.wwwapi8/Program.cs
+++ api-counter.wwwapi8/Program.cs
@@ -27,36 +27,60 @@ var counters = app.MapGroup("/counters");
 //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
 counters.MapGet("/", () =>
 {
-    return TypedResults.Ok();
+    return TypedResults.Ok(CounterHelper.Counters);
 });
 
 
 //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
 counters.MapGet("/{id}", (int id) =>
-{    
-    return TypedResults.Ok(id);
+{
+    var counter = CounterHelper.Counters.FirstOrDefault(x => x.Id == id);
+    return TypedResults.Ok(counter);
 });
 
 //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.        
 counters.MapGet("/greaterthan/{number}", (int number) =>
 {
-    return TypedResults.Ok(number);
+    var counterList = CounterHelper.Counters.Where(x => x.Value > number);
+    return TypedResults.Ok(counterList);
 });
 
 ////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
-
+counters.MapGet("/lessthan/{number}", (int number) =>
+{
+    var counterList = CounterHelper.Counters.Where(x => x.Value < number);
+    return TypedResults.Ok(counterList);
+});
 
 
 //Extension #1
 //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
 //return the counter you have increased
-
+counters.MapGet("/incrementvalue/{id}", (int id) =>
+{
+    var counter = CounterHelper.Counters.FirstOrDefault(x => x.Id == id);
+    if(counter != null)
+    {
+        counter.Value += 1;
+    }
+    return TypedResults.Ok(counter);
+});
 
 //Extension #2
 //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
 //return the counter you have decreased
+counters.MapGet("/decrementvalue/{id}", (int id) =>
+{
+    var counter = CounterHelper.Counters.FirstOrDefault(x => x.Id == id);
+    if (counter != null)
+    {
+        counter.Value -= 1;
+    }
+    return TypedResults.Ok(counter);
+});
+
 
 app.Run();
 
