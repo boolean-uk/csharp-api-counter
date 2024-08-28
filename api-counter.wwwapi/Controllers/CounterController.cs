e1feeeb68a56270d141e751ae35e9e32b9b21e00
Anders Hagen Ottersland <anderh0709@gmail.com>
2024-08-28 09:51:40 +0200

    Extentions in controllers done

--- api-counter.wwwapi/Controllers/CounterController.cs
+++ api-counter.wwwapi/Controllers/CounterController.cs
@@ -64,18 +64,41 @@ namespace api_counter.wwwapi.Controllers
             return TypedResults.Ok(countersLesserThan);
         }
 
-
-
-
         //Extension #1
         //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
         //return the counter you have increased
 
+        [HttpPatch]
+        [Route("increment/{id}")]
+        public async Task<IResult> PatchIncrementValue(int id)
+        {
+            var counterToIncrement = counters.FirstOrDefault(c => c.Id == id);
+            if(counterToIncrement == null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            counterToIncrement.Value += 1;
+            return TypedResults.Ok(counterToIncrement.Value);
+        }
 
         //Extension #2
         //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
         //return the counter you have decreased
+        [HttpPatch]
+        [Route("decrease/{id}")]
+        public async Task<IResult> PatchDecreaseValue(int id)
+        {
+            var counterToDecrease = counters.FirstOrDefault(c => c.Id == id);
+            if (counterToDecrease == null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            counterToDecrease.Value -= 1;
+            return TypedResults.Ok(counterToDecrease.Value);
+        }
     }
 }
