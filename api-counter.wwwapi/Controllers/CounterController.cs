f0b159b39c14cd42ff1420f2c209b1f7de2003d7
Victor <victor.chicinas@gmail.com>
2024-08-28 09:39:32 +0200

    Extemsions done

--- api-counter.wwwapi/Controllers/CounterController.cs
+++ api-counter.wwwapi/Controllers/CounterController.cs
@@ -64,7 +64,7 @@ namespace api_counter.wwwapi.Controllers
             var smaller = counters.Where(n => n.Value < number);
             return smaller != null ? TypedResults.Ok(smaller) : TypedResults.NotFound();
         }
-      
+
 
 
 
@@ -72,11 +72,41 @@ namespace api_counter.wwwapi.Controllers
         //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
         //return the counter you have increased
-        
+
+
+        [HttpGet]
+        [Route("increment/{id}")]
+        public async Task<IResult> GetIncrementValue(int id)
+        {
+            //write code here replacing the string.Empty
+            var counter = counters.FirstOrDefault(c => c.Id == id);
+            if (counter != null) {
+                counter.Value++;
+            }
+
+            //leave return line the same
+            return counter != null ? TypedResults.Ok(counter) : TypedResults.NotFound();
+        }
 
         //Extension #2
         //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
         //return the counter you have decreased
+
+        [HttpGet]
+        [Route("decremet/{id}")]
+        public async Task<IResult> GetDecrementValue(int id)
+        {
+            //write code here replacing the string.Empty
+            var counter = counters.FirstOrDefault(c => c.Id == id);
+            if (counter != null)
+            {
+                counter.Value--;
+            }
+
+            //leave return line the same
+            return counter != null ? TypedResults.Ok(counter) : TypedResults.NotFound();
+        }
     }
+
 }
