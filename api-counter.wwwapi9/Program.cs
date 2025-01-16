2999c1b44df791b398d81a79eed1eb918d552780
Nikolai Lillemark Børseth <nikolailb1999@gmail.com>
2025-01-16 13:20:49 +0100

    Completed all but super optional

--- api-counter.wwwapi9/Program.cs
+++ api-counter.wwwapi9/Program.cs
@@ -28,33 +28,45 @@ var counters = app.MapGroup("/counters");
 //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
 counters.MapGet("/", () =>
 {
-    return TypedResults.Ok();
+    return TypedResults.Ok(CounterHelper.Counters);
 });
 
 
 //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
 counters.MapGet("/{id}", (int id) =>
 {
-    return TypedResults.Ok(id);
+    return TypedResults.Ok(CounterHelper.Counters.Where(x => x.Id == id).First());
 });
 
 //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.        
 counters.MapGet("/greaterthan/{number}", (int number) =>
 {
-    return TypedResults.Ok(number);
+    return TypedResults.Ok(CounterHelper.Counters.Where(x => x.Value > number));
 });
 
 ////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
+counters.MapGet("/lessthan/{number}", (int number) =>
+{
+    return TypedResults.Ok(CounterHelper.Counters.Where(x => x.Value < number));
+});
 
 //Extension #1
 //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
 //return the counter you have increased
+counters.MapPost("/increment", (int id) =>
+{
+    return TypedResults.Ok(++CounterHelper.Counters.Where(x => x.Id == id).First().Value);
+});
 
 //Extension #2
 //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
 //return the counter you have decreased
+counters.MapPost("/decrement", (int id) =>
+{
+    return TypedResults.Ok(--CounterHelper.Counters.Where(x => x.Id == id).First().Value);
+});
 
 //Super Optional Extension #1
 //Refactor the code! include a repository layer (interface & concrete class), inject this into the endpoint.
